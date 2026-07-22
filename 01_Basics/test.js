ab4e5ecf1122423671ce0e79d1f6d9d5dcb251e9
AashuKushwaha <108549949+AashuKushwaha@users.noreply.github.com>
2026-07-22 17:08:41 +0000

    Variables

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,15 @@
-console.log('Aashu Kushwaha');
+const ID = "1";
+let Name = "Aashu";
+var EmailID = "aashu@gmail.com";
+accountCity ="Delhi";
+
+//ID =2;
+
+
+Name ="Priyanshi";
+EmailID="Priyanshi@gmail.com";
+accountCity="Etawah";
+console.log(ID);
+
+console.table([ID, Name, EmailID, accountCity]);
+
